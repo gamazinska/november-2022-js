d378bbe615865d66e5f094dd36306e996fef73cd
gamazinska <gamazinskaya86@mail.ru>
2022-11-03 18:11:18 +0200

    add com

---
+++ Homework1/main.js
@@ -0,0 +1,29 @@
+// Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+//    Вивести кожну змінну за допомогою: console.log
+
+
+// Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)
+
+//let firsName = 'Yuliya';
+//let middleName = 'Alexandrovna';
+//let lastName = 'Hamazinska';
+//let result = firsName + ' ' + middleName + ' ' + lastName;
+//console.log(result);
+//let result = `${firsName} ${middleName} ${lastName}`;
+//console.log(result);
+
+// За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+  //  let a = 100; let b = '100'; let c = true;
+
+let a = 100;
+console.log(typeof a);
+let b = '100';
+console.log(typeof b);
+let c = true;
+console.log(typeof c);
+
+
+//Додаткове для тих хто цікавився prompt`oм
+//За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками. та вивести в консоль
+
+
