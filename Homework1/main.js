8479cb5ca02a4b68deea63661dacdffe6315cac6
gamazinska <gamazinskaya86@mail.ru>
2022-11-03 19:05:25 +0200

    add comen

--- Homework1/main.js
+++ Homework1/main.js
@@ -1,14 +1,32 @@
 // Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
 //    Вивести кожну змінну за допомогою: console.log
+let hello = 'hello';
+console.log(hello) ;
+let num = 1;
+console.log(num);
+let bool1 = true;
+console.log(bool1);
+let bool2 = false;
+console.log(bool2);
+let PI = 3.14;
+console.log(PI);
+let str = 'own';
+console.log(str);
+
+hello = 'hello world';
+console.log(hello);
+
+bool1 = false;
+console.log(bool1);
 
 
 // Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)
 
-//let firsName = 'Yuliya';
-//let middleName = 'Alexandrovna';
-//let lastName = 'Hamazinska';
-//let result = firsName + ' ' + middleName + ' ' + lastName;
-//console.log(result);
+let firsName = 'Yuliya';
+let middleName = 'Alexandrovna';
+let lastName = 'Hamazinska';
+let result = firsName + ' ' + middleName + ' ' + lastName;
+console.log(result);
 //let result = `${firsName} ${middleName} ${lastName}`;
 //console.log(result);
 
@@ -26,4 +44,8 @@ console.log(typeof c);
 //Додаткове для тих хто цікавився prompt`oм
 //За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками. та вивести в консоль
 
-
+//let firstName = prompt('Name');
+//let middleName = prompt( 'middleName');
+//let age = +prompt ('Age');
+//let result = `${firstName} ${middleName} ${age}`;
+//console.log(result);
